2bf445f2e8a071dab49706ae6ab6f76dc3d4336a
lichenguang <477628869@qq.com>
2021-07-25 16:48:38 +0800

    更改数据库为mysql

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,8 +79,13 @@ WSGI_APPLICATION = 'bookmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : '125119',
+        # 'NAME': BASE_DIR / 'bookmanager',
+        'NAME': 'bookmanager',
     }
 }
 
