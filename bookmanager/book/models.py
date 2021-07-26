90949d548b34f84392e31ec12f5c43c7e8e883b9
lichenguang <477628869@qq.com>
2021-07-26 16:49:48 +0800

    数据库增删改查

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -4,13 +4,39 @@ from django.db import models
 
 class BookInfo(models.Model):
     # id
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10,unique=True)
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+
+    class Meta:
+        db_table='bookinfo'  #修改表的名字
+        verbose_name='书籍管理'  #admin站点使用的()了解
+
+
 
     def __str__(self):
         return self.name
 
 
 class PeopleInfo(models.Model):
-    name = models.CharField(max_length=10)
-    gender = models.BooleanField()
+
+    #定义一个有序字典   枚举类型
+    GENDER_CHOICE = (
+        (1,'male'),
+        (2,'female')
+    )
+
+    name = models.CharField(max_length=10, unique=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE,default=1)
+    description = models.CharField(max_length=100,null=True)
+    is_delete = models.BooleanField(default=False)
     book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+
+    def __str__(self):
+        return self.name
--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -12,3 +12,49 @@ def index(request):
     }
     # return HttpResponse("ok")
     return render(request, 'book/index.html', context)
+
+"""
+from book.models import BookInfo
+
+增加数据：
+方式1：
+book = BookInfo(
+        name='django',
+        pub_date='2000-1-1',
+        readcount=1
+)
+book.save()
+
+方式2：
+BookInfo.objects.create(
+        name='django',
+        pub_date='2000-1-1',
+        readcount=1
+)    
+
+
+更新数据：
+方式1：
+book = BookInfo.objects.ger(id=6)
+book.name = '哈哈哈'
+book.save()
+
+方式2：
+BookInfo.objects.filter(id=5).update(name='嘿嘿嘿', commentcount=888)
+
+
+删除数据：
+方式1：
+book=BookInfo.objects.get(id=2)
+book.delete()
+
+方式2：
+BookInfo.objects.get(id=2).delete()
+BookInfo.objects.filter(id=2).delete()
+"""
+
+
+
+
+
+
